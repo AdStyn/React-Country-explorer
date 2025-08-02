3be58280e426adff3bf7126ba895b89e450b7d35
AdStyn <kancell124@gmail.com>
2025-08-02 16:11:16 +0700

    dashboard

--- React Country Explorer/src/App.tsx
+++ React Country Explorer/src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
+import { Suspense } from "react";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <h1>React Country Explorer</h1>
-          <p>This is a simple React application to explore countries.</p>
-          <button onClick={() => setCount(count + 1)}>Count: {count}</button>
-        </header>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
     </>
   );
 }
